07dfe23c54eab5d157c268479fa154d3ba63b1df
Uy Nguyen <uy.nguyen@delta.com>
2022-12-06 13:06:16 -0500

    Day 1 pt 1 complete

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,36 @@ use std::path::Path;
 
 
 fn main() {
+    let mut calories: Vec<Vec<i32>> = Vec::new(); 
+    let mut _elf_calories: Vec<i32> = Vec::new();
+
+    // Build a vector of elf vector calories
     if let Ok(lines) = read_lines("./src/prob01.txt") {
         for line in lines {
             if let Ok(ip) = line {
-                println!("{}", ip);
+                if ip.is_empty() {
+                    calories.push(_elf_calories.clone());
+                    _elf_calories = Vec::new();
+                } else {
+                    let _calorie = ip.parse::<i32>().unwrap();
+                    _elf_calories.push(_calorie)
+                }
             }
         }
-    } else {
-        println!("Looking for file")
     }
+
+    let mut acc = calories[0].clone();
+    
+    let mut calories_per_elf = calories
+                                    .iter()
+                                    .map(|x| {x.iter().sum()})
+                                    .collect::<Vec<i32>>();
+        
+    let mut part_1_ans = calories_per_elf.clone().iter().max();
+
+    let mut part_2_ans = calories_per_elf.sort();
+    let sum: i32 = calories[0].clone().iter().sum();
+
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
