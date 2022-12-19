56e6344960702c61f57b32ea496081e94748c139
Uy Nguyen <nguyenuy@gmail.com>
2022-12-18 21:19:35 -0500

    updates

--- src/part05.rs
+++ src/part05.rs
@@ -1,30 +1,52 @@
 use std::collections::HashMap;
+use std::fs;
+use regex::Regex;
 
 use crate::utility;
 
 pub fn run_part_05() {
     println!("Running Day 05");
-    let crate_map: HashMap<usize, Vec<char>> = HashMap::new();
-    if let Ok(lines) = utility::read_lines("./src/problems/prob05.txt") {
-        for line in lines {
-            if let Ok(ip) = line {
-                let x: Vec<char> = ip
-                                    .replace('[', " ")
-                                    .replace(']', " ")
-                                    .chars()
-                                    .collect();
-                
-                let char_positions: Vec<usize> = (0 ..=(x.len() - 1)/4).map(|x| 4*x + 1).collect();
-                for (pos, e) in char_positions.iter().enumerate() {
-                    if x[*e] != ' ' {
-                    }
-                } 
-                
-            }
-        }
+    let mut crate_stacks: Vec<Vec<char>> = Vec::new();
+    let mut stacks: Vec<char> = Vec::new();
 
-        println!("Part 1 ans:");
-        println!("Part 2 ans:");
+    let contents = fs::read_to_string("./src/problems/prob05.txt").unwrap();
+    let problem: Vec<&str> = contents.split("\n").collect();
+    let problem_index = problem.iter().position(|&x| x.trim().is_empty()).unwrap();
+    
+    let crates: Vec<char> = problem[problem_index-1].chars().collect();
+    let mut crate_mapping: HashMap<char, Vec<char>> = HashMap::new();
+
+    for (pos, el) in crates.iter().enumerate() {
+        if *el != ' ' {
+            let mut crate_stack: Vec<char> = Vec::new();
+            for ind in (0 .. problem_index-1).rev() {
+                let crate_row: Vec<char> = problem[ind].chars().collect();
+                if crate_row[pos] != ' ' {
+                    crate_stack.push(crate_row[pos]);
+                } else {
+                    continue;
+                }
+            }
+            crate_mapping.insert(*el, crate_stack);
+        } else {
+            continue;
+        }
     }
+
+    let re = Regex::new(r"move (\d+) from (\d+) to (\d+)").unwrap();
+
+    for ind in (problem_index + 1 .. problem.len()) {
+        let command = problem[ind];
+        for cap in re.captures(command) {
+            let amount = &cap[1];
+            let src = &cap[2];
+            let dest = &cap[3];
+        }
+        
+    }
+
+    println!("Part 1 ans:");
+    println!("Part 2 ans:");
 }
+
 
